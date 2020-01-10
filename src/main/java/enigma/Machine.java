c65c0fcaea4e81af5c41f71d847e2f2be7502b86
martinjmil <50888704+martinjmil@users.noreply.github.com>
2020-01-10 14:20:35 +0100

    first commentaries of classes

--- src/main/java/enigma/Machine.java
+++ src/main/java/enigma/Machine.java
@@ -2,11 +2,13 @@ package enigma;
 
 public class Machine {
 
+        // attributs de la classe machine
 	private Rotor leftRotor;
 	private Rotor middleRotor;
 	private Rotor rightRotor;
 	private Reflector reflector;
-
+        
+        // Fonction d'initialisation de la place des rotors
 	public void initRotors(Reflector reflector, Rotor left, Rotor middle, Rotor right) {
 		this.reflector = reflector;
 		leftRotor = left;
@@ -14,6 +16,7 @@ public class Machine {
 		rightRotor = right;
 	}
 
+        // setter qui définie la position des rotors et du rélflecteur de la machine à partir d'une chaine de carractère
 	public void setPositions(String setting) {
 		char[] charSettings = setting.toCharArray();
 		reflector.setPosition(Rotor.toIndex(charSettings[0]));
@@ -21,13 +24,14 @@ public class Machine {
 		middleRotor.setPosition(Rotor.toIndex(charSettings[2]));
 		rightRotor.setPosition(Rotor.toIndex(charSettings[3]));
 	}
-	
+	// fonction qui définie la position des rotors avec la le setter "setPosition" après les avoir initialisé avec la fonction "initRotors"
 	public void configure(Reflector reflector, Rotor left, Rotor middle, Rotor right, String setting) {
 		this.initRotors(reflector, left, middle, right);
 		this.setPositions(setting);
 
 	}
 
+        //fonction pour convertir le massage en tableau de charactères majuscules avant de retounre ce tableau
 	public String convert(String msg) {
 		msg = msg.toUpperCase();
 		char[] msgChars = msg.toCharArray();
@@ -37,7 +41,7 @@ public class Machine {
 		}
 		return result;
 	}
-
+        // fonction qui chiffre le caractère du message en le faisant passer dans les rotors de la machine
 	char convertChar(char c) {
 		advanceRotors();
 		int charIndex = Rotor.toIndex(c);
@@ -52,11 +56,13 @@ public class Machine {
 		return Rotor.toLetter(output);
 
 	}
-
+        //fonction qui modifie la position des rotors en la faisant avancer les rotors 
 	void advanceRotors() {
 		boolean advanceLeft = false;
 		boolean advanceMiddle = false;
 		boolean advanceRight = true;
+                
+                // tests qui vérifie la présent d'un notch pour avancer les rotors correspondant d'une possition
 		if (leftRotor.atNotch()) {
 		}
 		if (middleRotor.atNotch()) {
@@ -67,6 +73,7 @@ public class Machine {
 			advanceMiddle = true;
 			advanceRight = true;
 		}
+                //fin des tests de la présence d'un notch
 		if (advanceLeft) {
 			leftRotor.advance();
 		}
